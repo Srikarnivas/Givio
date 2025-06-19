9f8ef7fecf90414f5903c57db04d2686e19317d3
Srikarnivas <srikarnivas.24@gmail.com>
2025-06-19 20:17:15 +0530

    Remove 'from email.headerregistry import Address' error import module

--- projects/Givio-contracts/smart_contracts/hello_world/contract.py
+++ projects/Givio-contracts/smart_contracts/hello_world/contract.py
@@ -1,4 +1,3 @@
-from email.headerregistry import Address
 from algopy import *
 from algopy.arc4 import abimethod
 
@@ -14,10 +13,10 @@ class Donation(ARC4Contract):
     def create(self, target: BigUInt, status: bool) -> None:
         assert target > 0
         self.creator_address = Txn.sender
-        self.total_donations = 0
+        self.total_donations = BigUInt(0)
         self.target = target
         self.campaign_active = status
-
+    
     @abimethod()
     def toggle_campaign(self, status: bool) -> None:
         assert Txn.sender == self.creator_address
@@ -26,7 +25,6 @@ class Donation(ARC4Contract):
     @abimethod()
     def get_campaign_details(self) -> tuple[Account, BigUInt, BigUInt, bool]:
         return self.creator_address, self.total_donations, self.target, self.campaign_active
-
 
     @abimethod()
     def is_target_reached(self) -> bool:
@@ -41,9 +39,39 @@ class Donation(ARC4Contract):
         assert self.total_donations+donateTxn.amount <= self.target
 
         self.total_donations += donateTxn.amount
-
+    
     @abimethod()
     def get_total_donations(self) -> BigUInt:
         return self.total_donations
     
+    @abimethod()
+    def withdraw(self, amount: UInt64) -> None:
+        assert self.creator_address == Txn.sender
+        assert self.total_donations >= amount
+        itxn.Payment(
+            amount = amount,
+            receiver= self.creator_address,
+            fee = 0
+        ).submit()
+        
+    @abimethod()
+    def get_remaining_target(self) -> BigUInt:
+        return self.target - self.total_donations
 
+    
+    @abimethod
+    def update_details(self, target: BigUInt, wallet: Account) -> None:
+        assert Txn.sender == self.creator_address
+        self.creator_address = wallet
+        self.target = target
+    
+    @abimethod(allow_actions=["DeleteApplication"])
+    def delete(self) -> None:
+        assert Txn.sender == self.creator_address
+
+        itxn.Payment(
+            receiver= self.creator_address,
+            amount = 0,
+            close_remainder_to= self.creator_address,
+            fee= 1_000,
+        ).submit()
